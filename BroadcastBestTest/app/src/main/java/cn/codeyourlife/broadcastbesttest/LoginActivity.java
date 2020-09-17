4f5c978ee4abadfee03049220df4447276e8dd4b
wbq813 <wbq813@foxmail.com>
2020-09-17 21:33:48 +0800

    [Update] broadcastBestTest

--- BroadcastBestTest/app/src/main/java/cn/codeyourlife/broadcastbesttest/LoginActivity.java
+++ BroadcastBestTest/app/src/main/java/cn/codeyourlife/broadcastbesttest/LoginActivity.java
@@ -3,9 +3,12 @@ package cn.codeyourlife.broadcastbesttest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -13,6 +16,8 @@ public class LoginActivity extends AppCompatActivity {
     private EditText nameEdit;
     private EditText passwdEdit;
     private Button login;
+    private CheckBox rememberPassword;
+    private SharedPreferences pref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,15 @@ public class LoginActivity extends AppCompatActivity {
         nameEdit = (EditText) findViewById(R.id.edit_name);
         passwdEdit = (EditText) findViewById(R.id.edit_passwd);
         login = (Button) findViewById(R.id.btn_login);
+        rememberPassword = (CheckBox) findViewById(R.id.remember_password);
+        pref = getPreferences(MODE_PRIVATE);
+
+        boolean isRemember = pref.getBoolean("remember_password", false);
+        if (isRemember) {
+            nameEdit.setText(pref.getString("name", ""));
+            passwdEdit.setText(pref.getString("password", ""));
+            rememberPassword.setChecked(isRemember);
+        }
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +43,17 @@ public class LoginActivity extends AppCompatActivity {
                 String name = nameEdit.getText().toString();
                 String passwd = passwdEdit.getText().toString();
                 if (name.equals("admin") && passwd.equals("123456")) {
+                    // 记住密码
+                    SharedPreferences.Editor  editor = pref.edit();
+                    if (rememberPassword.isChecked()) {
+                        editor.putBoolean("remember_password", true);
+                        editor.putString("name", name);
+                        editor.putString("password", passwd);
+                    }else{
+                        editor.clear();
+                    }
+                    editor.apply();
+
                     // 登录成功，跳转到main
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
