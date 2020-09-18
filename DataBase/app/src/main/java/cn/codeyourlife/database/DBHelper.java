987d74989a9bce942efce1595e5c80effd4e7963
wbq813 <wbq813@foxmail.com>
2020-09-18 11:46:44 +0800

    [Add] DataBaseTest

---
+++ DataBase/app/src/main/java/cn/codeyourlife/database/DBHelper.java
@@ -0,0 +1,45 @@
+package cn.codeyourlife.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public static final String TB_Book = "Book";
+    public static final String TB_Category = "Category";
+    public static final String CREATE_BOOK = "create table "+TB_Book+" (" +
+            "id integer primary key autoincrement, " +
+            "author text, " +
+            "price real, " +
+            "pages integer, " +
+            "name text)";
+
+    public static final String CREATE_CATEGORY = "create table "+TB_Category+" (" +
+            "id integer primary key autoincrement, " +
+            "category_name text, " +
+            "category_code integer)";
+
+    private Context context;
+
+    public DBHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREATE_BOOK);
+        sqLiteDatabase.execSQL(CREATE_CATEGORY);
+        Toast.makeText(context, "Create succeeded", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("drop table if exists "+TB_Book);
+        sqLiteDatabase.execSQL("drop table if exists "+TB_Category);
+        onCreate(sqLiteDatabase);
+    }
+}
---
+++ DataBase/app/src/main/java/cn/codeyourlife/database/MainActivity.java
@@ -0,0 +1,110 @@
+package cn.codeyourlife.database;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private DBHelper dbHelper;
+    public static final String DB_BOOK = "BookStore.db";
+    private TextView showTable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // db helper
+        this.dbHelper = new DBHelper(this, DB_BOOK, null, 2);
+        this.showTable = (TextView) findViewById(R.id.show_table);
+
+        // button create
+        Button btnCreateDb = (Button) findViewById(R.id.btn_create_db);
+        btnCreateDb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbHelper.getWritableDatabase();
+            }
+        });
+
+        // button insert
+        Button btnInsert = (Button) findViewById(R.id.btn_insert);
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("name", "The Da Vinci Code");
+                values.put("author", "Dan Brown");
+                values.put("pages", 454);
+                values.put("price", 16.96);
+                db.insert("Book", null, values);
+
+                // 第二条数据
+                values.put("name", "The Lost Symbol");
+                values.put("author", "Dan Brown");
+                values.put("pages", 510);
+                values.put("price", 19.95);
+                db.insert("Book", null, values);
+
+                Toast.makeText(MainActivity.this, "插入成功", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // button query
+        Button btnQuery = (Button) findViewById(R.id.btn_query);
+        btnQuery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                Cursor cursor = db.query("Book", null, null, null, null, null, null);
+                StringBuilder record= new StringBuilder();
+                if (cursor.moveToFirst()){
+                    do {
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price = cursor.getDouble(cursor.getColumnIndex("price"));
+                        record.append("<").append(name).append(">\t").append(author).append("\n")
+                                .append("Page: ").append(pages).append("\t$").append(price);
+                        record.append("\n");
+                    }while (cursor.moveToNext());
+                }
+                cursor.close();
+                // show data
+                showTable.setText(record.toString());
+            }
+        });
+
+        // btn update
+        Button btnUpdate = (Button) findViewById(R.id.btn_update);
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("price", 10.99);
+                db.update("Book", values, "name = ?", new String[]{"The Da Vinci Code"});
+                Toast.makeText(MainActivity.this, "更新成功", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // btn delete
+        Button btnDelete = (Button) findViewById(R.id.btn_delete);
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                db.delete("Book", "pages > ?", new String[]{"500"});
+                Toast.makeText(MainActivity.this, "删除成功", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
