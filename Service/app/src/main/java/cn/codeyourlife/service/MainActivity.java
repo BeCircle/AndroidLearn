dd541dee3e1ddbf0a1bee8b8bf517e0873396b24
wbq813 <wbq813@foxmail.com>
2020-09-29 22:35:25 +0800

    [Add] service test

---
+++ Service/app/src/main/java/cn/codeyourlife/service/MainActivity.java
@@ -0,0 +1,78 @@
+package cn.codeyourlife.service;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "MainActivity";
+
+    private MyService.DownloadBinder downloadBinder;
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            downloadBinder = (MyService.DownloadBinder) iBinder;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnStart = (Button) findViewById(R.id.btn_svc_start);
+        Button btnStop = (Button) findViewById(R.id.btn_svc_stop);
+        btnStart.setOnClickListener(this);
+        btnStop.setOnClickListener(this);
+
+        Button btnBind = (Button) findViewById(R.id.btn_svc_bind);
+        Button btnUnbind = (Button) findViewById(R.id.btn_svc_unbind);
+        btnBind.setOnClickListener(this);
+        btnUnbind.setOnClickListener(this);
+
+        Button btnIntentStart = (Button) findViewById(R.id.btn_int_svc_start);
+        btnIntentStart.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_svc_start:
+                Intent intent = new Intent(this, MyService.class);
+                startService(intent);
+                break;
+            case R.id.btn_svc_stop:
+                Intent intentStop = new Intent(this, MyService.class);
+                stopService(intentStop);
+                break;
+            case R.id.btn_svc_bind:
+                Intent intentBind = new Intent(this, MyService.class);
+                bindService(intentBind, connection, BIND_AUTO_CREATE);
+                break;
+            case R.id.btn_svc_unbind:
+                unbindService(connection);
+                break;
+            case R.id.btn_int_svc_start:
+                Log.d(TAG, "onClick: "+Thread.currentThread().getId());
+                Intent intentSvc = new Intent(this, MyIntentService.class);
+                startService(intentSvc);
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Service/app/src/main/java/cn/codeyourlife/service/MyService.java
@@ -0,0 +1,86 @@
+package cn.codeyourlife.service;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Binder;
+import android.os.Build;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+public class MyService extends Service {
+    private static final String TAG = "MyService";
+    private DownloadBinder mBinder = new DownloadBinder();
+
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate: ");
+
+        String id = "my_channel_01";
+        String name = "我是渠道名字";
+        Notification notification;
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, intent, 0);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel mChannel = new NotificationChannel(id, name, NotificationManager.IMPORTANCE_LOW);
+            NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            manager.createNotificationChannel(mChannel);
+
+            notification = new Notification.Builder(this, id)
+                    .setContentTitle("5 new messages")
+                    .setContentIntent(pi)
+                    .setAutoCancel(true)
+                    .setSmallIcon(R.mipmap.ic_launcher).build();
+        } else {
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, id)
+                    .setContentTitle("5 new messages")
+                    .setContentText("hahaha")
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentIntent(pi)
+                    .setAutoCancel(true)
+                    .setOngoing(true);
+
+            notification = notificationBuilder.build();
+        }
+        startForeground(1, notification);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand: ");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    class DownloadBinder extends Binder {
+        public void startDownload(){
+            Log.d(TAG, "startDownload: ");
+        }
+        public int getProgress(){
+            Log.d(TAG, "getProgress: ");
+            return 0;
+        }
+    }
+}
